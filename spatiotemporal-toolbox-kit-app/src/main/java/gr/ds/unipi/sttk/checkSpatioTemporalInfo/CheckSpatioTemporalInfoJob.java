69296fa9554a1fcbe9cddceec5a2a816b5dda170
nikolaos <nnikos_koutroumanis@hotmail.com>
2019-10-22 18:19:43 +0300

    changes made

--- spatiotemporal-toolbox-kit-app/src/main/java/gr/ds/unipi/sttk/checkSpatioTemporalInfo/CheckSpatioTemporalInfoJob.java
+++ spatiotemporal-toolbox-kit-app/src/main/java/gr/ds/unipi/sttk/checkSpatioTemporalInfo/CheckSpatioTemporalInfoJob.java
@@ -2,7 +2,8 @@ package gr.ds.unipi.sttk.checkSpatioTemporalInfo;
 
 public class CheckSpatioTemporalInfoJob {
     public static void main(String args[]) throws Exception {
-
+        AppConfig config = AppConfig.newAppConfig(args[0]);
+        config.getCheckSpatioTemporalInfo(config.getRecordParser(config.getDataSource()));
     }
 
 }
