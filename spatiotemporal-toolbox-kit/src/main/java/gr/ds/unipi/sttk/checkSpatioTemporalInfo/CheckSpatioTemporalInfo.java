86d2ff44962752950cd4f8240b045573d041dfa3
Nicholas Koutroumanis <nikos_koutroumanis@hotmail.com>
2019-10-26 18:55:07 +0300

    CheckSpatioTemporalInfo class remade

--- spatiotemporal-toolbox-kit/src/main/java/gr/ds/unipi/sttk/checkSpatioTemporalInfo/CheckSpatioTemporalInfo.java
+++ spatiotemporal-toolbox-kit/src/main/java/gr/ds/unipi/sttk/checkSpatioTemporalInfo/CheckSpatioTemporalInfo.java
@@ -21,19 +21,16 @@ public final class CheckSpatioTemporalInfo {
     private static final Logger logger = LoggerFactory.getLogger(CheckSpatioTemporalInfo.class);
 
     private final RecordParser recordParser;
-
     private final Rectangle rectangle;
-
-    private Set<String> errorLines;
-    //private Set<String> emptySpatialInformation;
-    private Set<String> spatioTemporalInformationOutOfRange;
-
     private long numberOfRecords = 0;
 
     private double maxx = Integer.MIN_VALUE;
     private double minx = Integer.MAX_VALUE;
     private double maxy = Integer.MIN_VALUE;
     private double miny = Integer.MAX_VALUE;
+
+    private Date minDate = new Date(Long.MAX_VALUE);
+    private Date maxDate = new Date(Long.MIN_VALUE);
 
     private CheckSpatioTemporalInfo(Builder builder) {
         recordParser = builder.recordParser;
@@ -44,62 +41,10 @@ public final class CheckSpatioTemporalInfo {
         return new CheckSpatioTemporalInfo.Builder(recordParser);
     }
 
-
-//    @Override
-//    public void emptySpatioTemporalInformation(Path file, String lineWithMeta) {
-//
-//        if (filesWithErrors.contains("Empty Spatitemporal Information " + file.toString())) {
-//            filesWithErrors.add("Empty Spatiotemporal Information " + file.toString());
-//        } else {
-//            filesWithErrors.add("Empty Spatitemporal Information " + file.toString());
-//        }
-//    }
-
-//    @Override
-//    public void outOfRangeSpatialInformation(Path file, String lineWithMeta) {
-//
-//        if (filesWithErrors.contains("Out of Range Spatial Information " + file.toString())) {
-//            filesWithErrors.add("Out of Range Spatial Information " + file.toString());
-//        } else {
-//            filesWithErrors.add("Out of Range Spatial Information " + file.toString());
-//        }
-//    }
-//
-//    @Override
-//    public void lineParse(String lineWithMeta, String[] separatedLine, int numberOfColumnLongitude, int numberOfColumnLatitude, int numberOfColumnDate, double longitude, double latitude) {
-//
-//        if (Double.compare(maxx, longitude) == -1) {
-//            maxx = longitude;
-//        }
-//        if (Double.compare(minx, longitude) == 1) {
-//            minx = longitude;
-//        }
-//        if (Double.compare(maxy, latitude) == -1) {
-//            maxy = latitude;
-//        }
-//        if (Double.compare(miny, latitude) == 1) {
-//            miny = latitude;
-//        }
-//
-//        numberOfRecords++;
-//
-//    }
-
-
-//    @Override
-//    public void lineWithError(Path file, String lineWithMeta) {
-//        if (filesWithErrors.contains("Lines with Errors " + file.toString())) {
-//            filesWithErrors.add("Lines with Errors " + file.toString());
-//        } else {
-//            filesWithErrors.add("Lines with Errors " + file.toString());
-//        }
-//    }
-
     public void exportInfo(FileOutput fileOutput) throws IOException, ParseException {
 
-        errorLines = new HashSet<>();
-        //emptySpatialInformation = new HashSet<>();
-        spatioTemporalInformationOutOfRange = new HashSet<>();
+        Set<String> errorLines = new HashSet<>();
+        Set<String> spatioTemporalInformationOutOfRange = new HashSet<>();
 
         DateFormat dateFormat = new SimpleDateFormat(recordParser.getDateFormat());
 
@@ -140,20 +85,14 @@ public final class CheckSpatioTemporalInfo {
                     miny = latitude;
                 }
 
+                if (maxDate.compareTo(d) < 0) {
+                    maxDate = d;
+                }
+                if (minDate.compareTo(d) > 0) {
+                    minDate = d;
+                }
+
                 numberOfRecords++;
-
-
-//                if (Datasource.empty.test(separatedLine[numberOfColumnLongitude - 1]) || Datasource.empty.test(separatedLine[numberOfColumnLatitude - 1]) || Datasource.empty.test(separatedLine[numberOfColumnDate - 1])) {
-//
-//                    if (parser instanceof KafkaDatasource) {
-//                        a[1] = a[1].substring(0, a[1].lastIndexOf("."));
-//                    }
-//
-//                    if (!emptySpatialInformation.contains(a[1])) {
-//                        emptySpatialInformation.add(a[1]);
-//                    }
-//                    continue;
-//                }
 
 
             } catch (NumberFormatException | ParseException | ArrayIndexOutOfBoundsException e) {
@@ -172,19 +111,6 @@ public final class CheckSpatioTemporalInfo {
                     errorLines.add(lineMetaData);
                 }
             }
-
-//            catch (ArrayIndexOutOfBoundsException | NumberFormatException | ParseException e) {
-//
-//                if (parser instanceof KafkaDatasource) {
-//                    a[1] = a[1].substring(0, a[1].lastIndexOf("."));
-//                }
-//
-//                if (!errorLines.contains(a[1])) {
-//                    errorLines.add(a[1]);
-//                }
-//
-//                continue;
-//            }
 
         }
 
@@ -208,47 +134,13 @@ public final class CheckSpatioTemporalInfo {
         fileOutput.out("Max Latitude: " + maxy, fileName);
         fileOutput.out("Min Latitude: " + miny, fileName);
 
+        fileOutput.out("Max Date: " + dateFormat.format(maxDate), fileName);
+        fileOutput.out("Min Date: " + dateFormat.format(minDate), fileName);
+
         fileOutput.out("\r\n", fileName);
         fileOutput.out("All of the records are " + numberOfRecords, fileName);
 
         fileOutput.close();
-
-
-//        filesWithErrors = new HashSet<>();
-//
-//        parse(filesPath, separator, filesExtension, numberOfColumnLongitude, numberOfColumnLatitude, numberOfColumnDate);
-//
-//        //create Export Directory
-//        try {
-//            Files.createDirectories(Paths.get(txtExportPath));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//
-//        try (FileOutputStream fos = new FileOutputStream(txtExportPath + File.separator + "SpatiotemporalFilesInfo.txt", true);
-//             OutputStreamWriter osw = new OutputStreamWriter(fos, "utf-8"); BufferedWriter bw = new BufferedWriter(osw); PrintWriter pw = new PrintWriter(bw, true);) {
-//
-//            pw.write("Files With Errors:" + "\r\n");
-//            filesWithErrors.forEach((s) -> pw.write(s + "\r\n"));
-//            pw.write("\r\n");
-//
-//            pw.write("Formed Spatial Box: " + "\r\n");
-//            pw.write("Max Longitude: " + maxx + "\r\n");
-//            pw.write("Min Longitude: " + minx + "\r\n");
-//            pw.write("Max Latitude: " + maxy + "\r\n");
-//            pw.write("Min Latitude: " + miny + "\r\n");
-//
-//            pw.write("\r\n");
-//            pw.write("All of the records are " + numberOfRecords + "\r\n");
-//
-//        } catch (UnsupportedEncodingException e) {
-//            e.printStackTrace();
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
 
     }
 
@@ -273,6 +165,5 @@ public final class CheckSpatioTemporalInfo {
         }
 
     }
-
 
 }
