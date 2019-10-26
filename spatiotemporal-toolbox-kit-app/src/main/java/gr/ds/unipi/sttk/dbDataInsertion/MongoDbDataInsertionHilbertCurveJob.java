ac66ad55acc5a63f8fbb49a215addbb113060f82
Nicholas Koutroumanis <nikos_koutroumanis@hotmail.com>
2019-10-27 00:48:29 +0300

    error fixed

--- spatiotemporal-toolbox-kit-app/src/main/java/gr/ds/unipi/sttk/dbDataInsertion/MongoDbDataInsertionHilbertCurveJob.java
+++ spatiotemporal-toolbox-kit-app/src/main/java/gr/ds/unipi/sttk/dbDataInsertion/MongoDbDataInsertionHilbertCurveJob.java
@@ -15,7 +15,7 @@ public final class MongoDbDataInsertionHilbertCurveJob {
 
     private static MongoDbDataInsertionHilbertCurve getMongoDbInsertionHilbertCurveJob(Config config, RecordParser recordParser) throws Exception {
         Config mongodb = config.getConfig("mongodb");
-        Config space = config.getConfig("space");
+        Config space = mongodb.getConfig("space");
         Config filter = config.getConfig("filter");
         MongoDbDataInsertionHilbertCurve.Builder mongoDbDataInsertion = MongoDbDataInsertionHilbertCurve.newMongoDbDataInsertionHilbertCurve(mongodb.getString("host"),mongodb.getInt("port"),mongodb.getString("database"),mongodb.getString("username"),mongodb.getString("password") ,mongodb.getString("collection"), mongodb.getInt("batchSize"),recordParser, mongodb.getInt("bits"), Rectangle.newRectangle(space.getDouble("minLon"),space.getDouble("minLat"),space.getDouble("maxLon"),space.getDouble("maxLat")),mongodb.getString("minDate"), mongodb.getString("maxDate"));
         if(mongodb.getBoolean("filter")){
