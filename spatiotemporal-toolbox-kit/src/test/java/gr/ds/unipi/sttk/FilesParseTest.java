6a8f97752eaddf720c8295322da263300a04f788
Nicholas Koutroumanis <nikos_koutroumanis@hotmail.com>
2019-11-27 13:57:54 -0300

    FilesParseTest.java modified

--- spatiotemporal-toolbox-kit/src/test/java/gr/ds/unipi/sttk/FilesParseTest.java
+++ spatiotemporal-toolbox-kit/src/test/java/gr/ds/unipi/sttk/FilesParseTest.java
@@ -33,17 +33,13 @@ public class FilesParseTest {
 
     @Test
     public void harvesine() {
-        int bits = 2;
+        int bits = 3;
         long maxOrdinates = 1L << bits;
 
 
         SmallHilbertCurve c = HilbertCurve.small().bits(bits).dimensions(3);
 
-        //System.out.println(maxOrdinates);
-        //long index = c.index(scalePoint(4,5,5,maxOrdinates));
-        //System.out.println(index);
-
-        Ranges rangesList = c.query(new long[]{3,3,5},new long[]{8,10,6}, 3);
+        Ranges rangesList = c.query(new long[]{3,3,5}, new long[]{8,10,6}, 0);
 
         rangesList.stream().forEach(i->{
             System.out.println(i);
