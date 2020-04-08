f106de9829445592c8129a4b46336ef29fc9c917
Nicholas Koutroumanis <nikos_koutroumanis@hotmail.com>
2020-04-08 23:36:31 +0300

    statistics

--- spatiotemporal-toolbox-kit-app/src/main/java/gr/ds/unipi/sttk/statistics/StatisticsJob.java
+++ spatiotemporal-toolbox-kit-app/src/main/java/gr/ds/unipi/sttk/statistics/StatisticsJob.java
@@ -15,7 +15,6 @@ public final class StatisticsJob {
         CsvRecordParser csvRecordParser = new CsvRecordParser(FileDatasource.newFileDatasource("/mnt/sdb/vfi2-enriched/",".csv"), ";", 7,8,3,"yyyy-MM-dd'T'HH:mm:ss.SSS");
 
         Statistics.newStatistics().filter(Rectangle.newRectangle(-180, -90, 180, 90)).build().calculateElementsFromCSVformat(csvRecordParser, Arrays.asList(49,50,51,52,53,54,55,56,57,58,59,60,61), FileOutput.newFileOutput("/home/nikolaos/Documents/statistikaNick/",false));
-
     }
 
 }
